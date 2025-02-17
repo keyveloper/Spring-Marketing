0926e8824c290db1c64a0b1e9568e5882f5201d3
zen <didehdrb21@naver.com>
2025-02-17 22:31:40 +0900

    marketing project starts

---
+++ marketing/src/main/kotlin/org/example/marketing/MarketingApplication.kt
@@ -0,0 +1,11 @@
+package org.example.marketing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketingApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketingApplication>(*args)
+}
---
+++ marketing/src/main/kotlin/org/example/marketing/UserController.kt
@@ -0,0 +1,4 @@
+package org.example.marketing
+
+class UserController {
+}
