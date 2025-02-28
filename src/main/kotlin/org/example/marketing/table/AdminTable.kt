3de21ae32773518ec36935866ef5a0af00320da1
zen <didehdrb21@naver.com>
2025-02-28 18:06:36 +0900

    admin table rebuilding

--- src/main/kotlin/org/example/marketing/table/AdminTable.kt
+++
@@ -1,9 +0,0 @@
-package org.example.marketing.table
-
-import org.jetbrains.exposed.dao.id.LongIdTable
-import org.jetbrains.exposed.sql.Column
-
-object AdminTable: BaseLongIdTable("admins") {
-    val loginId: Column<String> = varchar("login_id", 255)
-    val password: Column<String> = varchar("password", 255)
-}
