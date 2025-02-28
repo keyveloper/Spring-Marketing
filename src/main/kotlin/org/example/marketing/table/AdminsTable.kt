6206d7ada7a834ea328d1bbed0157e6d6344c891
zen <didehdrb21@naver.com>
2025-02-28 18:30:15 +0900

    adminstable rebuilding

--- src/main/kotlin/org/example/marketing/table/AdminsTable.kt
+++ src/main/kotlin/org/example/marketing/table/AdminsTable.kt
@@ -3,7 +3,7 @@ package org.example.marketing.table
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.Column
 
-object AdminTable: BaseLongIdTable("admins") {
-    val loginId: Column<String> = varchar("login_id", 255)
+object AdminsTable: BaseLongIdTable("admins") {
+    val loginId: Column<String> = varchar("login_id", 255).uniqueIndex()
     val password: Column<String> = varchar("password", 255)
 }
