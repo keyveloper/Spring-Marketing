687f62572df103175807d25fdd38b8020dca46f6
zen <didehdrb21@naver.com>
2025-04-15 23:55:19 +0900

    match field with scrapper server

--- src/main/kotlin/org/example/marketing/dto/keyword/ScrappedDataFromScrapperServer.kt
+++ src/main/kotlin/org/example/marketing/dto/keyword/ScrappedDataFromScrapperServer.kt
@@ -3,5 +3,5 @@ package org.example.marketing.dto.keyword
 data class ScrappedDataFromScrapperServer(
     val keyword: String,
     val blogVisitStat: List<BlogVisitStat>,
-    val monthlyMeaningFulPostingBlogCount: Int,
+    val monthlyMeaningfulPostingBlogCount: Int,
 )
