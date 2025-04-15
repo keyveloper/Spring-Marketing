b8dccfbc7e3f1ad277d14bce52151ee8c55730c9
zen <didehdrb21@naver.com>
2025-04-15 23:56:32 +0900

    log response body

--- src/main/kotlin/org/example/marketing/controller/GoldenKeywordController.kt
+++ src/main/kotlin/org/example/marketing/controller/GoldenKeywordController.kt
@@ -1,5 +1,6 @@
 package org.example.marketing.controller
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.validation.Valid
 import org.example.marketing.dto.keyword.*
 import org.example.marketing.enums.FrontErrorCode
@@ -13,15 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 class GoldenKeywordController(
     private val goldenKeywordService: GoldenKeywordService
 ) {
+    private val logger = KotlinLogging.logger {}
     @GetMapping("/test/keywords/golden")
     suspend fun getGoldenKeyword(
         @Valid @RequestBody request: GetGoldenKeywordsRequest
     ): ResponseEntity<GetGoldenKeywordsResponse> {
+        val goldenKeywords = goldenKeywordService.getGoldenKeywords(request)
+        logger.info { "response body: $goldenKeywords" }
         return ResponseEntity.ok().body(
             GetGoldenKeywordsResponse.of(
                 frontErrorCode = FrontErrorCode.OK.code,
                 errorMessage = FrontErrorCode.OK.message,
-                goldenKeywords = goldenKeywordService.getGoldenKeywords(request)
+                goldenKeywords = goldenKeywords
             )
         )
     }
