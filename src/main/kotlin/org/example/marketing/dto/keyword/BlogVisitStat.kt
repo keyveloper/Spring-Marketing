f5ab2815b86cbf9b984f7fea41959a26251f120a
zen <didehdrb21@naver.com>
2025-04-16 21:53:21 +0900

    bloggerId become nullable - cause : post..

--- src/main/kotlin/org/example/marketing/dto/keyword/BlogVisitStat.kt
+++ src/main/kotlin/org/example/marketing/dto/keyword/BlogVisitStat.kt
@@ -1,11 +1,11 @@
 package org.example.marketing.dto.keyword
 
 data class BlogVisitStat(
-    private val title: String,
-    private val bloggerId: String,
-    private val blogUrl: String,
-    private val rank: Int,
-    private val avg5dVisitCount: Int,
-    private val max5dVisitCount: Int,
-    private val min5dVisitCount: Int
+    val title: String,
+    val bloggerId: String?, // POST 떄문에 ... ㅊㅇ
+    val blogUrl: String,
+    val rank: Int,
+    val avg5dVisitCount: Int,
+    val max5dVisitCount: Int,
+    val min5dVisitCount: Int
 )
