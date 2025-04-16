84d13ee4b3a0babdcf168123b24647d80e0e3e88
zen <didehdrb21@naver.com>
2025-04-16 21:52:50 +0900

    advertiser auth login logic added

---
+++ src/main/kotlin/org/example/marketing/service/UserPrincipalService.kt
@@ -0,0 +1,34 @@
+package org.example.marketing.service
+
+import org.example.marketing.domain.user.AdminPrincipal
+import org.example.marketing.domain.user.AdvertiserPrincipal
+import org.example.marketing.domain.user.CustomUserPrincipal
+import org.example.marketing.exception.InvalidUserTypeException
+import org.example.marketing.repository.user.AdminRepository
+import org.example.marketing.repository.user.AdvertiserRepository
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.stereotype.Service
+
+@Service
+class UserPrincipalService(
+    private val adminRepository: AdminRepository,
+    private val advertiserRepository: AdvertiserRepository
+    // add other user repository
+) {
+    fun loadUserByTypeAndLoginId(type: String, loginId: String) : CustomUserPrincipal = transaction {
+        when (type) {
+            "ADMIN" -> {
+                val admin = adminRepository.findByLoginId(loginId)
+                AdminPrincipal.of(admin)
+            }
+
+            "ADVERTISE" -> {
+                val advertiser = advertiserRepository.findByLoginId(loginId)
+                AdvertiserPrincipal.of(advertiser)
+            }
+            else -> throw InvalidUserTypeException(logics = "UserPrincipalService-loadBy...")
+        }
+    }
+
+    // add other fun
+}
