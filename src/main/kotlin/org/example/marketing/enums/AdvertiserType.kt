daf7561cd4fa536a39f1767a54d3ac5022272308
zen <didehdrb21@naver.com>
2025-04-16 21:57:25 +0900

    type for normal and brand

---
+++ src/main/kotlin/org/example/marketing/enums/AdvertiserType.kt
@@ -0,0 +1,6 @@
+package org.example.marketing.enums
+
+enum class AdvertiserType {
+    NORMAL,
+    BRAND
+}
---
+++ src/main/kotlin/org/example/marketing/enums/UserStatus.kt
@@ -0,0 +1,7 @@
+package org.example.marketing.enums
+
+enum class UserStatus {
+    LIVE,
+    EXPIRED, // 탈퇴만
+    DEAD  // real dead
+}
