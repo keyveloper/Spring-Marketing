d71874f4d11afdc8c11ada30421025e5e772c4c3
zen <didehdrb21@naver.com>
2025-04-16 21:57:42 +0900

    principal for advertiser

---
+++ src/main/kotlin/org/example/marketing/domain/user/AdvertiserPrincipal.kt
@@ -0,0 +1,19 @@
+package org.example.marketing.domain.user
+
+import org.example.marketing.dao.user.AdvertiserEntity
+import org.example.marketing.enums.UserType
+
+data class AdvertiserPrincipal(
+    override val userId: Long, // entity id : Loog
+    override val loginId: String,
+    override val userType: UserType = UserType.ADVERTISER
+): CustomUserPrincipal {
+    companion object {
+        fun of(admin: AdvertiserEntity): AdvertiserPrincipal {
+            return AdvertiserPrincipal(
+                userId = admin.id.value,
+                loginId = admin.loginId,
+            )
+        }
+    }
+}
