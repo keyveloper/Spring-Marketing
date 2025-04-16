d4091e081ad8b98e0daca51161bd835cd17bcc50
zen <didehdrb21@naver.com>
2025-04-16 21:53:40 +0900

    advertiser login logic added

--- src/main/kotlin/org/example/marketing/service/AuthService.kt
+++ src/main/kotlin/org/example/marketing/service/AuthService.kt
@@ -1,9 +1,11 @@
 package org.example.marketing.service
 
 import org.example.marketing.dto.user.request.LoginAdminRequest
+import org.example.marketing.dto.user.request.LoginAdvertiserRequest
 import org.example.marketing.enums.UserType
 import org.example.marketing.exception.PasswordNotMatchedException
 import org.example.marketing.repository.user.AdminRepository
+import org.example.marketing.repository.user.AdvertiserRepository
 import org.example.marketing.security.JwtTokenProvider
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -13,7 +15,8 @@ import org.springframework.stereotype.Service
 class AuthService(
     private val adminRepository: AdminRepository,
     private val jwtTokenProvider: JwtTokenProvider,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val advertiserRepository: AdvertiserRepository
 ) {
     fun loginAdmin(request: LoginAdminRequest): String = transaction {
         val admin = adminRepository.findByLoginId(request.loginId)
@@ -23,6 +26,17 @@ class AuthService(
         }
 
         val jwtToken = jwtTokenProvider.generateToken(admin.loginId, UserType.ADMIN)
+        jwtToken
+    }
+
+    fun loginAdvertiser(request: LoginAdvertiserRequest): String = transaction {
+        val advertiser = advertiserRepository.findByLoginId(request.loginId)
+
+        if (!passwordEncoder.matches(request.password, advertiser.password)) {
+            throw PasswordNotMatchedException(logics = "authService- loginAdvertiser")
+        }
+
+        val jwtToken = jwtTokenProvider.generateToken(advertiser.loginId, UserType.ADVERTISER)
         jwtToken
     }
 
