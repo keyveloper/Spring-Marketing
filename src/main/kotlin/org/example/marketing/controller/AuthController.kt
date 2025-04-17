44cd247c4b49c80ae958feebdfa7a89f1aa497a5
zen <didehdrb21@naver.com>
2025-04-17 22:03:20 +0900

    add advertiser login

--- src/main/kotlin/org/example/marketing/controller/AuthController.kt
+++ src/main/kotlin/org/example/marketing/controller/AuthController.kt
@@ -3,11 +3,13 @@ package org.example.marketing.controller
 import org.example.marketing.domain.user.CustomUserPrincipal
 import org.example.marketing.dto.user.ValidateTokenResponse
 import org.example.marketing.dto.user.request.LoginAdminRequest
+import org.example.marketing.dto.user.request.LoginAdvertiserRequest
 import org.example.marketing.dto.user.response.LoginAdminResponse
+import org.example.marketing.dto.user.response.LoginAdvertiserResponse
 import org.example.marketing.enums.FrontErrorCode
 import org.example.marketing.service.AuthService
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.http.ResponseEntity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AuthController(
     private val authService: AuthService,
-    private val userDetailsServiceAutoConfiguration: UserDetailsServiceAutoConfiguration
+    private val webSecurityConfiguration: WebSecurityConfiguration
 ) {
 
     @PostMapping("/test/login/admin")
@@ -28,6 +30,19 @@ class AuthController(
                 FrontErrorCode.OK.code,
                 FrontErrorCode.OK.message,
                 authService.loginAdmin(request)
+            )
+        )
+    }
+
+    @PostMapping("/test/entry/advertiser")
+    fun loginAdvertiser(
+        @RequestBody request: LoginAdvertiserRequest
+    ): ResponseEntity<LoginAdvertiserResponse> {
+        return ResponseEntity.ok().body(
+            LoginAdvertiserResponse.of(
+                FrontErrorCode.OK.code,
+                FrontErrorCode.OK.message,
+                authService.loginAdvertiser(request)
             )
         )
     }
