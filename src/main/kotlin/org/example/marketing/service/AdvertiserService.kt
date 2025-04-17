f4c3fc74e7c9509226ed74640729ddf51ba10e2d
zen <didehdrb21@naver.com>
2025-04-18 00:13:20 +0900

    advertiser profile api added

--- src/main/kotlin/org/example/marketing/service/AdvertiserService.kt
+++ src/main/kotlin/org/example/marketing/service/AdvertiserService.kt
@@ -1,8 +1,11 @@
 package org.example.marketing.service
 
 import org.example.marketing.domain.user.Advertiser
+import org.example.marketing.domain.user.AdvertiserProfile
 import org.example.marketing.dto.user.request.MakeNewAdvertiserRequest
 import org.example.marketing.dto.user.request.SaveAdvertiser
+import org.example.marketing.dto.user.response.AdvertiserProfileResult
+import org.example.marketing.repository.user.AdvertiserProfileRepository
 import org.example.marketing.repository.user.AdvertiserRepository
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service
 @Service
 class AdvertiserService(
     private val advertiserRepository: AdvertiserRepository,
+    private val advertiserProfileRepository: AdvertiserProfileRepository,
     private val passwordEncoder: PasswordEncoder
 ) {
     fun save(request: MakeNewAdvertiserRequest): Long = transaction {
@@ -24,5 +28,15 @@ class AdvertiserService(
     fun findUserInfo(id: Long): Advertiser = transaction {
         val advertiserEntity = advertiserRepository.findById(id)
         Advertiser.of(advertiserEntity)
+    }
+
+    fun findProfiledById(advertiserId: Long): AdvertiserProfileResult = transaction {
+        val advertiserProfileEntity = advertiserProfileRepository.findByAdvertiserId(advertiserId)
+        val advertiserEntity = advertiserRepository.findById(advertiserId)
+
+        AdvertiserProfileResult.of(
+            advertiserEntity,
+            advertiserProfileEntity
+        )
     }
 }
