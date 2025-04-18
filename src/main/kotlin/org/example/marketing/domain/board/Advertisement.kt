ac6f713f5b555f3c1a88f3779f5f8d8c0e2f52b4
zen <didehdrb21@naver.com>
2025-04-18 15:25:51 +0900

    add advertiser delivery data class

--- src/main/kotlin/org/example/marketing/domain/board/Advertisement.kt
+++ src/main/kotlin/org/example/marketing/domain/board/Advertisement.kt
@@ -3,7 +3,9 @@ package org.example.marketing.domain.board
 import org.example.marketing.config.CustomDateTimeFormatter
 import org.example.marketing.dao.board.AdvertisementEntity
 import org.example.marketing.dao.board.AdvertisementLocationEntity
+import org.example.marketing.dto.board.response.AdvertisementDeliveryCategories
 import org.example.marketing.enums.ChannelType
+import org.example.marketing.enums.DeliveryCategory
 import org.example.marketing.enums.ReviewType
 
 sealed class Advertisement {
@@ -27,7 +29,9 @@ sealed class Advertisement {
         val updatedAt: String,
     ) : Advertisement() {
         companion object {
-            fun of(entity: AdvertisementEntity): AdvertisementGeneral {
+            fun of(
+                entity: AdvertisementEntity,
+            ): AdvertisementGeneral {
                 return AdvertisementGeneral(
                     id = entity.id.value,
                     title = entity.title,
@@ -44,13 +48,59 @@ sealed class Advertisement {
                     siteUrl = entity.siteUrl,
                     itemInfo = entity.itemInfo,
                     createdAt = CustomDateTimeFormatter.epochToString(entity.createdAt),
-                    updatedAt = CustomDateTimeFormatter.epochToString(entity.updatedAt)
+                    updatedAt = CustomDateTimeFormatter.epochToString(entity.updatedAt),
                 )
             }
-
-
         }
     }
+
+    data class AdvertisementDelivery(
+        val id: Long,
+        val title: String,
+        val reviewType: ReviewType,
+        val channelType: ChannelType,
+        val recruitmentNumber: Int,
+        val itemName: String,
+        val recruitmentStartAt: String,
+        val recruitmentEndAt: String,
+        val announcementAt: String,
+        val reviewStartAt: String,
+        val reviewEndAt: String,
+        val endAt: String,
+        val siteUrl: String?,
+        val itemInfo: String?,
+        val createdAt: String,
+        val updatedAt: String,
+        val categories: List<DeliveryCategory>
+    ) : Advertisement() {
+        companion object {
+            fun of(
+                entity: AdvertisementEntity,
+                category: AdvertisementDeliveryCategories
+            ): AdvertisementDelivery {
+                return AdvertisementDelivery(
+                    id = entity.id.value,
+                    title = entity.title,
+                    reviewType = entity.reviewType,
+                    channelType = entity.channelType,
+                    recruitmentNumber = entity.recruitmentNumber,
+                    itemName = entity.itemName,
+                    recruitmentStartAt = CustomDateTimeFormatter.epochToString(entity.recruitmentStartAt),
+                    recruitmentEndAt = CustomDateTimeFormatter.epochToString(entity.recruitmentEndAt),
+                    announcementAt = CustomDateTimeFormatter.epochToString(entity.announcementAt),
+                    reviewStartAt = CustomDateTimeFormatter.epochToString(entity.reviewStartAt),
+                    reviewEndAt = CustomDateTimeFormatter.epochToString(entity.reviewEndAt),
+                    endAt = CustomDateTimeFormatter.epochToString(entity.endAt),
+                    siteUrl = entity.siteUrl,
+                    itemInfo = entity.itemInfo,
+                    createdAt = CustomDateTimeFormatter.epochToString(entity.createdAt),
+                    updatedAt = CustomDateTimeFormatter.epochToString(entity.updatedAt),
+                    categories = category.categories
+                )
+            }
+        }
+    }
+
 
     data class AdvertisementWithLocation(
         val locationEntityId: Long,
