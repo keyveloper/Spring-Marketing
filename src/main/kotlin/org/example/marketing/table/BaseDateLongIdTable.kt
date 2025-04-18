8a6c1add26f79d92c3881fd47369f2f94e11fa4a
zen <didehdrb21@naver.com>
2025-04-18 15:26:22 +0900

    name changed

--- src/main/kotlin/org/example/marketing/table/BaseDateLongIdTable.kt
+++ src/main/kotlin/org/example/marketing/table/BaseDateLongIdTable.kt
@@ -2,10 +2,8 @@ package org.example.marketing.table
 
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.javatime.datetime
-import java.time.LocalDateTime
 
-abstract class BaseLongIdTable(tableName: String, idName: String = "id") : LongIdTable(tableName, idName) {
+abstract class BaseDateLongIdTable(tableName: String, idName: String = "id") : LongIdTable(tableName, idName) {
     val createdAt: Column<Long> = long("created_at").clientDefault { System.currentTimeMillis() / 1000 } // second
     val updatedAt: Column<Long> = long("updated_at").clientDefault { System.currentTimeMillis() / 1000 }
 }
